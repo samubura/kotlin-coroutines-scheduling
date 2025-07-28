e272d1baa33fd434151421a1848aeafe013c802a
Samuele Burattini <samuele.burattini@unibo.it>
2025-07-28 18:50:50 +0200

    create a variant of pingpong that uses the +belief argument instead of retrieving a value from beliefs directly

---
+++ src/main/kotlin/agent/examples/PingPongArgumentsMAS.kt
@@ -0,0 +1,66 @@
+package agent.examples
+
+import agent.AchieveEvent
+import agent.Agent
+import agent.BasicMapEnvironment
+import agent.EnvironmentContext
+import agent.Plan
+import agent.agent
+import agent.args
+import agent.environment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import kotlin.coroutines.coroutineContext
+
+private val pinger = Agent(
+    "Pinger",
+    listOf(
+        Plan("start") {
+            val agent = coroutineContext.agent
+            val env = coroutineContext.environment as BasicMapEnvironment
+            val x = 0
+            env.set("ping", x)
+            agent.say("Ping ${x}!")
+        },
+        Plan("+pong") {
+            val agent = coroutineContext.agent
+            val env = coroutineContext.environment as BasicMapEnvironment
+            delay(500)
+            // TODO the first argument of a belief update plan is the belief value
+            val x = coroutineContext.args[0] as Int + 1
+            env.set("ping", x)
+            agent.say("Ping ${x}!")
+        }
+    ),
+    listOf(
+        AchieveEvent("start")
+    )
+)
+
+private val ponger = Agent(
+    "Ponger",
+    listOf(
+        Plan("+ping") {
+            val agent = coroutineContext.agent
+            val env = coroutineContext.environment as BasicMapEnvironment
+            delay(500)
+            val x = coroutineContext.args[0] as Int + 1
+            env.set("pong", x)
+            agent.say("Pong ${x}!")
+        }
+    )
+)
+
+suspend fun main() = supervisorScope {
+    val agents = listOf(pinger, ponger)
+    val env = BasicMapEnvironment(agents)
+
+    val masContext = coroutineContext + EnvironmentContext(env)
+
+    agents.forEach { agent ->
+        launch (masContext) {
+            agent.run()
+        }
+    }
+}
